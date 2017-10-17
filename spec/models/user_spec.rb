a1c4d180f512a56c1c25192dce1120c003dfd79a
akiraandy <andypschwartz@gmail.com>
2017-10-17 14:04:36 -0500

    add and pass user spec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,5 +2,18 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should have_secure_password }
-  
+
+  it { should validate_presence_of(:name) }
+
+  it { should validate_presence_of(:password) }
+
+  it { should validate_presence_of(:email) }
+
+  it { should have_many(:workouts).dependent(:destroy) }
+
+  it { should have_many(:exercises) }
+
+  it { should have_many(:sets) }
+
+
 end
